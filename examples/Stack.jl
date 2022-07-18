592d9b6cd9f1866f440858d18877c9012793a499
Brady Planden <info@bradyplanden.com>
2022-07-18 18:47:04 +0100

    merge commit + multicell

---
+++ examples/Stack.jl
@@ -0,0 +1,79 @@
+using BattCalc
+
+Stack = Params(Anode(),Cathode(),Separator())
+Layers = 20
+Cell = Pouch(Stack, "NCM811","Graphite","1Li:0.8Ni:0.1Co:0.1Mn:2O","1.0Li6.0C", "Exper", "Intercalation", Layers)
+Module = MultiCell(Stack, Cell, N=200, M=20)
+
+
+
+
+
+#Intercalation
+@with_kw mutable struct Cathode
+    Porousity = (0.61±0.0122)u"cm^3/cm^3"
+    CoatingThickness = (45.0±0.9)u"μm"
+    CollectorThickness = (13±0.26)u"μm"
+    Area = (85±1.7)u"cm^2"
+end
+
+@with_kw mutable struct Anode
+    Porousity = (0.2±0.004)u"cm^3/cm^3"
+    CoatingThickness = (65±2)u"μm"
+    CollectorThickness = (8±0.16)u"μm"
+    Area = (85±1.7)u"cm^2"
+end
+
+@with_kw mutable struct Separator
+    Porousity = (0.61±0.0122)u"cm^3/cm^3"
+    Thickness = (14.0±0.28)u"μm"
+    Area = (85±1.7)u"cm^2"
+    Loading = (1.01±0.0202)u"mg/cm^3"
+end
+
+
+
+#Deposition - AF
+@with_kw mutable struct Cathode
+    Porousity = (0.61±0.0122)u"cm^3/cm^3"
+    CoatingThickness = (45.0±0.9)u"μm"
+    CollectorThickness = (13±0.26)u"μm"
+    Area = (85±1.7)u"cm^2"
+end
+
+@with_kw mutable struct Anode
+    Porousity = (0.0±0.0)u"cm^3/cm^3"
+    CoatingThickness = (17.0±0.34)u"μm"
+    CollectorThickness = (8±0.16)u"μm"
+    Area = (85±1.7)u"cm^2"
+end
+
+@with_kw mutable struct Separator
+    Porousity = (0.61±0.0122)u"cm^3/cm^3"
+    Thickness = (14.0±0.28)u"μm"
+    Area = (85±1.7)u"cm^2"
+    Loading = (1.01±0.0202)u"mg/cm^3"
+end
+
+
+#Deposition - Excess Lithium
+@with_kw mutable struct Cathode
+    Porousity = (0.61±0.0122)u"cm^3/cm^3"
+    CoatingThickness = (45.0±0.9)u"μm"
+    CollectorThickness = (13±0.26)u"μm"
+    Area = (85±1.7)u"cm^2"
+end
+
+@with_kw mutable struct Anode
+    Porousity = (0.0±0.0)u"cm^3/cm^3"
+    CoatingThickness = (34.0±0.68)u"μm"
+    CollectorThickness = (8±0.16)u"μm"
+    Area = (85±1.7)u"cm^2"
+end
+
+@with_kw mutable struct Separator
+    Porousity = (0.61±0.0122)u"cm^3/cm^3"
+    Thickness = (14.0±0.28)u"μm"
+    Area = (85±1.7)u"cm^2"
+    Loading = (1.01±0.0202)u"mg/cm^3"
+end
